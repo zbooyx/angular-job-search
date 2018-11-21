18f79bce7ef5fe75e27c8c9e8644d42d7f9eb307
zbooyx <zbooyx@gmail.com>
2018-11-21 17:00:50 +0100

    caterpillar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {ControlContainer, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {OffersComponent} from './offers/offers.component';
 import {HeaderComponent} from './header/header.component';
-import {LoginFormComponent} from './login-form/login-form.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
 import {RouterModule, Routes} from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
+import {HomeComponent} from './home/home.component';
 
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: LoginFormComponent
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  }
-];
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent,
     OffersComponent,
     HeaderComponent,
-    LoginFormComponent,
-    DashboardComponent,
-    LoginComponent,
     HomeComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
@@ -43,9 +26,7 @@ const appRoutes: Routes = [
     MaterialModule,
     ReactiveFormsModule,
   ],
-  providers: [
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
