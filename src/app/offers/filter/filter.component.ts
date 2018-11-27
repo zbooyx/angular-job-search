ba6dd63975a901791a4303f8b43a4378fa8598e8
zbooyx <zbooyx@gmail.com>
2018-11-27 18:45:11 +0100

    fix and style filter.component

--- src/app/offers/filter/filter.component.ts
+++ src/app/offers/filter/filter.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent  {
+export class FilterComponent {
 
   first = true;
   second = false;
+  third = true;
+  fourth = false;
+  hideList = false;
+
 
   constructor() {
 
   }
+
   public metoda123() {
     this.first = !this.first;
     this.second = !this.second;
-    console.log(this.first);
+    this.third = !this.third;
+    this.fourth = !this.fourth;
+    console.log(this.third);
   }
 }
 
