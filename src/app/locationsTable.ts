02db6008b0a17ed2e7acbcd0727ffacc78c2e453
zbooyx <zbooyx@gmail.com>
2018-11-29 10:08:30 +0100

    feat create locationsTable.ts with array

---
+++ src/app/locationsTable.ts
@@ -0,0 +1,47 @@
+const StateAndCity =[
+  {state: string, city: string}
+  {state: 'dolnośląskie', city: 'Bolesławice'},
+  {state: 'dolnośląskie', city: 'Dzierżoniów'},
+  {state: 'dolnośląskie', city: 'Głogów'},
+  {state: 'dolnośląskie', city: 'Jelenia Góra'},
+  {state: 'dolnośląskie', city: 'Jelenia Góra'},
+
+  {state: 'kujawsko-pomorskie, city: Bydgoszcz'},
+  {state: 'kujawsko-pomorskie, city: Toruń'},
+  {state: 'kujawsko-pomorskie, city: Grudziądz'},
+  {state: 'kujawsko-pomorskie, city: Włocławek'},
+
+  {state: 'lubelskie', city: 'Chełm'},
+  {state: 'lubelskie', city: 'Świdnica'},
+  {state: 'lubelskie', city: 'Zamość'},
+
+  {state: 'łódzkie', city: 'Łódź'},
+  {state: 'łódzkie', city: 'Pabianice'},
+  {state: 'łódzkie', city: 'Opoczno'},
+  {state: 'łódzkie', city: 'Skierniewice'},
+
+  {state: 'małopolskie', city: 'Gorlice'},
+  {state: 'małopolskie', city: 'Tarnów'},
+  {state: 'małopolskie', city: 'Nowy Sącz'},
+  {state: 'małopolskie', city: 'Kraków'},
+
+  {state: 'mazowieckie', city: 'Ostrołęka'},
+  {state: 'mazowieckie', city: 'Płock'},
+  {state: 'mazowieckie', city: 'Radom'},
+  {state: 'mazowieckie', city: 'Warszawa'},
+
+  {state: 'opolskie', city: 'Opole'},
+
+  {state: 'pomorskie', city: 'Elbląg'},
+  {state: 'pomorskie', city: 'Gdańsk'},
+  {state: 'pomorskie', city: 'Kwidzyn'},
+
+
+  {state: 'podkarpackie', city:},
+  {state: 'podlaskie', city:},
+  {state: 'świętokrzyskie', city:},
+  {state: 'śląskie', city:},
+  {state: 'warmińsko-mazurskie', city:},
+  {state: 'wielkopolskie', city:},
+  {state: 'zachodniopomorskie', city:},
+];
