96fd40a825c58d58031e290613701a28d315bd52
zbooyx <zbooyx@gmail.com>
2018-11-29 16:16:23 +0100

    feat(): create offers.config.ts

---
+++ src/config/offers.config.ts
@@ -0,0 +1,131 @@
+export const Offers = [
+  {
+    title: 'fryzjer',
+    employer: 'hairDead',
+    location: 'Kielce',
+    logoURL: 'ddd',
+    salary: '2222',
+    date: '07/11/2018'
+  },
+  {
+    title: 'pianista',
+    employer: 'music OK',
+    location: 'Gdańsk',
+    logoURL: 'ddd',
+    salary: '2355',
+    date: '01/11/2018'
+  },
+  {
+    title: 'kreślarz',
+    employer: 'CAD office',
+    location: 'Kielce',
+    logoURL: 'ddd',
+    salary: '2666',
+    date: '07/11/2018'
+  },
+  {
+    title: 'fryzjer',
+    employer: 'Your Hair',
+    location: 'Iława',
+    logoURL: 'ddd',
+    salary: '1800',
+    date: '01/11/2018'
+  },
+  {
+    title: 'mechanik',
+    employer: 'dreed',
+    location: 'Pruszcz Gdański',
+    logoURL: 'ddd',
+    salary: '4222',
+    date: '11/11/2018'
+  },
+  {
+    title: 'kierowkca',
+    employer: 'music OK',
+    location: 'Gdańsk',
+    logoURL: 'ddd',
+    salary: '4233',
+    date: '11/11/2018'
+  },
+  {
+    title: 'kucharz',
+    employer: 'kuchnia TV',
+    location: 'Gdański',
+    logoURL: 'ddd',
+    salary: '3900',
+    date: '11/11/2018'
+  },
+  {
+    title: 'reporter',
+    employer: 'music OK',
+    location: 'Gdańsk',
+    logoURL: 'ddd',
+    salary: '750',
+    date: '19/11/2018'
+  },
+  {
+    title: 'kelner',
+    employer: 'kuchnia TV',
+    location: 'Gdańsk',
+    logoURL: 'ddd',
+    salary: '750',
+    date: '19/11/2018'
+  },
+  {
+    title: 'kelner',
+    employer: 'Garnek',
+    location: 'Kraków',
+    logoURL: 'ddd',
+    salary: '5750',
+    date: '11/11/2018'
+  },
+  {
+    title: 'mechanik',
+    employer: 'TooL',
+    location: 'Elbląg',
+    logoURL: 'ddd',
+    salary: '2750',
+    date: '30/11/2018'
+  },
+  {
+    title: 'operator koparki',
+    employer: 'TooL',
+    location: 'Elbląg',
+    logoURL: 'ddd',
+    salary: '1750',
+    date: '04/11/2018'
+  },
+  {
+    title: 'mechanik',
+    employer: 'Garaż',
+    location: 'Kraków',
+    logoURL: 'ddd',
+    salary: '5550',
+    date: '04/11/2018'
+  },
+  {
+    title: 'Księgowa',
+    employer: 'money&money',
+    location: 'Toruń',
+    logoURL: 'ddd',
+    salary: '11150',
+    date: '04/12/2018'
+  },
+  {
+    title: 'inzynier budowy',
+    employer: 'budROCK',
+    location: 'Chełm',
+    logoURL: 'ddd',
+    salary: '6550',
+    date: '02/11/2018'
+  },
+  {
+    title: 'data manager',
+    employer: 'money&money',
+    location: 'Toruń',
+    logoURL: 'ddd',
+    salary: '7550',
+    date: '02/11/2018'
+  }
+
+];
